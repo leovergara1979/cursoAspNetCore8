7105130523283153e2edb791ef3b1a89cc956061
Leonardo Vergara R. <leovergara@gmail.com>
2024-02-09 15:08:33 -0300

    curso net core 8

---
+++ netcore8/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace netcore8.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
